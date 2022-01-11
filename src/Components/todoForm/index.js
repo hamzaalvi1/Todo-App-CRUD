4a0ce64d994ef581b57d1da76f14e891741bbcb5
hamzaalvi1 <hamzaalvi224@gmail.com>
2022-01-11 12:57:14 +0500

    Working on form

--- src/Components/todoForm/index.js
+++ src/Components/todoForm/index.js
@@ -1,10 +1,19 @@
 import React,{useState} from 'react'
 
 const TodoForm = () => {
+   
     const [todoData,setTodoData]= useState({todoTitle: "",todoDetails:""})
+     
+    const formSubmit = (evt)=>{
+      evt.preventDefault()
+      console.log(todoData)   
+      setTodoData({todoTitle: "",todoDetails:""})
+     
+    }
+  
     return (
         <div className='todoForm'>
-            <form className='main_Form'>
+            <form className='main_Form' onSubmit={(evt)=>formSubmit(evt)} >
                 <div className="frm-fld">
                    
                     <input type="text" placeholder = "Todo Title" name='todoTitle' 
